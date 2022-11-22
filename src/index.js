2c4176f6ba6bc24cf32187c6cd290c18bc5df8fe
abdessittir harkaty <abdessattarharkaty@gmail.com>
2022-11-22 11:36:57 +0100

    Add homepage and fetch data from the API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './styles/global.css';
 
 import footer from './modules/footer';
+import main from './modules/main';
 // list of movies: `${process.env.MOVIES_URL}?api_key=${process.env.API_KEY}`
 
 // movie detail: `${process.env.MOVIES_URL}/${movieId}?api_key=${process.env.API_KEY}`
@@ -9,6 +10,9 @@ import footer from './modules/footer';
 
 const render = () => {
   const footerEle = document.querySelector('.footer');
+  const mainEle = document.getElementById('main');
+
+  mainEle.insertAdjacentHTML('beforeend', main());
   footerEle.replaceChildren('');
   footerEle.insertAdjacentHTML('beforeend', footer());
 };
