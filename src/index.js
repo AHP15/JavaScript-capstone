1174af73c5aebb93f986ad2bd3a52bce4cdee060
Yonas44 <tesfu.yonas1350@gmail.com>
2022-11-22 17:18:36 +0300

    Resolve merge conflict

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import './styles/global.css';
 import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
 import '../node_modules/@fortawesome/fontawesome-free/js/all.js';
 
-import footer from './modules/footer.js';
-import Header from './modules/header.js';
+import footer from './modules/footer';
+import Header from './modules/header';
 // list of movies: `${process.env.MOVIES_URL}?api_key=${process.env.API_KEY}`
 
 // movie detail: `${process.env.MOVIES_URL}/${movieId}?api_key=${process.env.API_KEY}`
