bac50ebd070a381d55acb2b37b4af192461a38f6
Yonas44 <tesfu.yonas1350@gmail.com>
2022-11-22 16:05:02 +0300

    Add header to the app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import './styles/global.css';
+import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
+import '../node_modules/@fortawesome/fontawesome-free/js/all.js';
 
-import footer from './modules/footer';
+import footer from './modules/footer.js';
+import Header from './modules/header.js';
 // list of movies: `${process.env.MOVIES_URL}?api_key=${process.env.API_KEY}`
 
 // movie detail: `${process.env.MOVIES_URL}/${movieId}?api_key=${process.env.API_KEY}`
@@ -8,9 +11,27 @@ import footer from './modules/footer';
 // movie image: `${process.env.IMAGE_URL}/imageId.jpg`
 
 const render = () => {
+  const header = document.querySelector('.header');
+  header.innerHTML = Header();
   const footerEle = document.querySelector('.footer');
   footerEle.replaceChildren('');
   footerEle.insertAdjacentHTML('beforeend', footer());
 };
 
 render();
+
+const navMenuOpen = document.querySelector('.open');
+const navMenuClose = document.querySelector('.close');
+const list = document.querySelector('.list');
+
+navMenuOpen.addEventListener('click', () => {
+  list.classList.add('on');
+  navMenuOpen.style.display = 'none';
+  navMenuClose.style.display = 'block';
+});
+
+navMenuClose.addEventListener('click', () => {
+  list.classList.remove('on');
+  navMenuOpen.style.display = 'block';
+  navMenuClose.style.display = 'none';
+});
