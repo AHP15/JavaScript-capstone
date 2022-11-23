ac19edbdd4fada07fb5593874debdef029a61a11
Yonas44 <tesfu.yonas1350@gmail.com>
2022-11-23 09:48:37 +0300

    Add fix styling

--- src/modules/displayModal.js
+++ src/modules/displayModal.js
@@ -18,7 +18,7 @@ const displayModal = (details) => {
   }" />
     <svg class="svg-inline--fa fa-xmark fa-3x modal-close" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="xmark" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" data-fa-i2svg=""><path fill="currentColor" d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"></path></svg>
   </div>
-  <div class="movie-info">
+  <div class="movie-information">
     <h2 id="title">${details.title}</h2>
     <div class="desc">
       <p><strong>Released:</strong> ${details.release_date}</p>
@@ -29,6 +29,7 @@ const displayModal = (details) => {
       <p><strong>Ratings:</strong> ${details.vote_average}</p>
     </div>
     <div class="comments">
+     <h3 id="comment-title">Comments</h3>
       <ul class="list-of-comments">
         <li>I really enjoyed the movie</li>
         <li>yeah same here</li>
