674ab9a516ca3cf097fa9f1603866997358c4c5b
abdessittir harkaty <abdessattarharkaty@gmail.com>
2022-11-23 13:57:29 +0100

    Add likes functionality

--- src/modules/Movie.js
+++ src/modules/Movie.js
@@ -10,32 +10,19 @@ const sketelon = () => `
 </div>
 `;
 
-const handleHeartClick = () => {
-
-};
-
 const movie = (movie = null) => {
   if (!movie) {
     return sketelon();
   }
 
   const image = movie?.poster_path ?? movie?.backdrop_path;
-  let { likes } = movie;
 
   window.addEventListener('movies_loaded', () => {
-    const likeBtns = document.querySelectorAll('.like-btn');
-    const likeNumbers = document.querySelectorAll('.likes-number');
-
-    likeBtns.forEach((btn, i) => {
-      btn.addEventListener('click', () => {
-        const element = likeNumbers[i];
-        /*
-        console.log(Number(element.textContent) + 1);
-        element.textContent = Number(element.textContent) + 1;
-        */
-        console.log(element, likeNumbers);
-        handleHeartClick(movie.id);
-      });
+    const likeBtn = document.getElementById(`img_${movie.id}`);
+    const likeNumber = document.getElementById(`number_${movie.id}`);
+    likeBtn.addEventListener('click', () => {
+      likeNumber.textContent = Number(likeNumber.textContent) + 1;
+      addLike(movie.id);
     });
   });
 
@@ -49,8 +36,8 @@ const movie = (movie = null) => {
           <h4>${movie.original_title.slice(0, 15)}...</h4>
 
           <div class="likes-container">
-            <img class="like-btn" src="${heart}" alt="like ${movie.original_title}" />
-            <p><span class="likes-number">${likes}</span> likes</p>
+            <img id="img_${movie.id}" src="${heart}" alt="like ${movie.original_title}" />
+            <p><span id="number_${movie.id}">${movie.likes}</span> likes</p>
           </div>
         </div>
 
