4bec3744bfc62febd67727cfcd91f8a18cf7d26f
Yonas44 <tesfu.yonas1350@gmail.com>
2022-11-24 12:23:36 +0300

    Fix linters

--- src/modules/commentCounter.js
+++ src/modules/commentCounter.js
@@ -2,6 +2,6 @@ const commentCounter = () => {
   const commentItem = document.querySelectorAll('.comment-item');
   const commentTitle = document.querySelector('#comment-title');
   commentTitle.innerHTML = `Comments(${commentItem.length})`;
-}
+};
 
 exports.commentCounter = commentCounter;
